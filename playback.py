637629abd1642b57c225ff3b9637df18bd5944eb
Advika3 <212727010+Advika3@users.noreply.github.com>
2025-09-16 19:24:59 +0530

    Create playback.py

---
+++ playback.py
@@ -0,0 +1,6 @@
+x= input("Enter your input:")
+print('...'.join(x.split()))
+'''
+implement a program in Python that prompts the user for input and then outputs that same input,
+replacing each space with ... (i.e., three periods).
+'''
