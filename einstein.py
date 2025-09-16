080783129770264866695c2cadf5be887135f6e7
Advika3 <212727010+Advika3@users.noreply.github.com>
2025-09-16 19:21:37 +0530

    Create einstein.py

---
+++ einstein.py
@@ -0,0 +1,14 @@
+x=int(input("m: "))
+
+def square(n):
+    return pow(n,2)
+E=x*(square(300000000))
+print(f"E: {E}")
+'''
+x=int(input("Enter mass value in [E=mc^2]:"))
+
+def square(n):
+    return pow(n,2)
+E=x*(square(300000000))
+print(f"Answer is:{E}")
+'''
