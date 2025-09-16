51069e24546e017d79dbc49279997681c0802658
Advika3 <212727010+Advika3@users.noreply.github.com>
2025-09-16 19:22:19 +0530

    Create extensions.py

---
+++ extensions.py
@@ -0,0 +1,21 @@
+x=input("File:").lower().strip()
+if x.endswith('jpg'):
+        print("image/jpeg")
+elif x.endswith('gif'):
+        print("image/gif")
+elif x.endswith('jpeg'):
+        print("image/jpeg")
+elif x.endswith('pdf'):
+        print("application/pdf")
+elif x.endswith('txt'):
+        print("text/plain")
+elif x.endswith('zip'):
+        print("application/zip")
+elif x.endswith('png'):
+        print("image/png")
+elif x.endswith('bin'):
+        print("application/octet-stream")
+elif x.endswith('pdf'):
+        print("application/pdf")
+elif x.endswith('myfile'):
+        print("application/octet-stream")
