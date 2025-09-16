d7c5ec93f3a921636f8588dfff40d22c5e0580ba
Advika3 <212727010+Advika3@users.noreply.github.com>
2025-09-16 19:23:59 +0530

    Create indoor.py

---
+++ indoor.py
@@ -0,0 +1,11 @@
+x=input("Enter your input:").lower()
+print(x)
+'''
+GIVEN
+implement a program in Python that prompts the user
+for input and then outputs that same input in lowercase.
+Punctuation and whitespace should be outputted unchanged.
+FOR MY REF
+In your terminal, execute the below to submit your work.
+submit50 cs50/problems/2022/python/indoor
+'''
